cdbff68f31af57b701732bc453f8135bf55f14ef
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2026-05-09 14:30:18 -0300

    feat(Categoria): agregar getter y setter para la clase Categoria + sobreescribir método toString para mostrar los datos de la clase

--- src/com/techlab/articulo/model/Categoria.java
+++ src/com/techlab/articulo/model/Categoria.java
@@ -32,21 +32,54 @@ public class Categoria implements Identificable {
     private String nombre;
     private String descripcion;
 
-    // TODO:
     // Crear constructor.
-
-    @Override
-    public int getCodigo() {
-        // TODO:
-        return 0;
+    public Categoria(int codigo, String nombre, String descripcion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 
-    // TODO:
     // Crear getters y setters restantes.
+    @Override
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
     @Override
     public String toString() {
-        // TODO:
-        return "";
+        // Mostrar todos los datos comunes del artículo.
+        // Sugerencia:
+        // incluir también tipo y precio final.
+        return """
+                Categoria{
+                  codigo='%s'
+                  nombre='%s'
+                  descripcion=%s
+                  categoria=%s
+                  tipo=%s
+                }""".formatted(
+                codigo,
+                nombre,
+                descripcion);
     }
 }
